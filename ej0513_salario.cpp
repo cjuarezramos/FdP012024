760b3c6d81de6b5002fb6355d389668f598da511
Carlos Juarez <cjuarez@uca.edu.sv>
2024-05-13 12:36:15 -0600

    Ejercicio de salario del 13 de mayo

--- ej0513_salario.cpp
+++ ej0513_salario.cpp
@@ -1,2 +1,61 @@
 #include <iostream>
+using namespace std;
+int main ()
+{
+    // declaramos varibles
+    float horas = 0;
+    float horas_b, horas_e, salario_neto,impuesto,salario_liquido;
+    string nombre;
 
+    // datos de entrada:
+    cout << "Ingrese nombre: ";
+    cin >> nombre;
+    cout << "Ingrese horas trabajas en el mes: ";
+    cin >> horas;
+
+    // Calculo de salirio neto, sin impuestos
+    if (horas<=160 && horas>0)
+    {
+        horas_b = horas;
+        horas_e = 0;
+    }
+    else
+    {
+        horas_b = 160;
+        horas_e = horas - 160;
+    }
+    salario_neto = horas_b * 10 + horas_e * 10 * 1.5;
+    
+    if (salario_neto<=2000)
+    {
+        impuesto = salario_neto * 0;
+    }
+
+    else if (salario_neto>2000 && salario_neto<2200)
+    {
+        impuesto = salario_neto * 0.2;
+
+    }
+    else
+    {
+        impuesto = salario_neto * 0.3;
+    }
+    
+    // salidas
+    cout << "Hola, " << nombre << " su datos de salario son: \n";
+    cout << "\tSalario neto:\t" << salario_neto;
+    cout << "\n\tImpuesto:\t" << impuesto;
+    cout << "\n\tSalario Liquido:\t" << salario_neto-impuesto << "\n";
+     
+    
+    
+    //cout << salario_neto << "\n";
+
+
+
+
+
+
+
+    return 0;
+}
