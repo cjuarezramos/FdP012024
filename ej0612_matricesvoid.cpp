038633cf53a5c63bf47adb1ddef3c3bb6893aaf4
Carlos Juarez <cjuarez@uca.edu.sv>
2024-06-12 11:53:38 -0600

    actualizacion de ejercicio de 12 de junio

--- ej0612_matricesvoid.cpp
+++ ej0612_matricesvoid.cpp
@@ -18,6 +18,33 @@ int main()
     cin >> m;
     string nombres[m]; // nombres de estudiantes
     float notas[m][n]; // arreglo de dos dimensiones para guardar notas de estudiantes.
+    // pedir nombres y notas
+    float notafinal[m];
+    for (int i=0;i<m;i++)
+    {
+        cout << "Nombre del estudiante " << i+1 << ": ";
+        cin >> nombres[i];
+        cout << nombres[i] <<" P1: ";
+        cin >> notas[i][0];
+        cout << nombres[i] <<" P2: ";
+        cin >> notas[i][1];
+        cout << nombres[i] <<" C1: ";
+        cin >> notas[i][2];
+        cout << nombres[i] <<" C2: ";
+        cin >> notas[i][3];
+        cout << nombres[i] <<" Lab: ";
+        cin >> notas[i][4];
+        cout << nombres[i] <<" Proyecto: ";
+        cin >> notas[i][5];
+    }
+    notafinal = calculo_notafin(notas,Ponderaciones);
+
+    for (int i=0;i<n;i++)
+    {
+        cout << nombres[i] << " su nota final es: " << notafinal[i] << "\n";
+    }
+
+
 
 
     instruccion();
