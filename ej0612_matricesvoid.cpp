de8ce134dd37e2268cd00439a352b09a48d9018f
Carlos Juarez <cjuarez@uca.edu.sv>
2024-06-12 12:29:02 -0600

    final del ejercico del 12 de junio

--- ej0612_matricesvoid.cpp
+++ ej0612_matricesvoid.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+constexpr int n = 7; // las notas por alumno y para fdp son 6;
 // Funcion de indicaciones
 void instruccion(void)
 {
@@ -8,45 +9,57 @@ void instruccion(void)
     cout << "\ntambien esta el uso de arreglos de multiples dimensiones";
     cout << "\nEl ejemplo es calculo de nota final para varios estudiantes\n";
 }
+
+float *calculo_notafin(float notas[][n], float ponderaciones[], int m)
+{
+    //float notafinal[m];
+    for (int j = 0; j < m; j++)
+    {
+        notas[j][6] = 0;
+        for (int i = 0; i < n-1; i++)
+        {
+            notas[j][6] = notas[j][6] + notas[j][i] * ponderaciones[i];
+        }
+    }
+}
+
 int main()
 {
+    instruccion();
     // Variables que requiero en el programa
     int m; // cantidad de alumnos
-    int n = 6; // las notas por alumno y para fdp son 6;
-    float Ponderaciones[n]={0.15,0.2,0.1,0.1,0.2,0.25}; // las ponderaciones de cada nota de FdP
+
+    float Ponderaciones[n] = {0.15, 0.2, 0.1, 0.1, 0.2, 0.25}; // las ponderaciones de cada nota de FdP
     cout << "Cuantos alumnos son: ";
     cin >> m;
     string nombres[m]; // nombres de estudiantes
     float notas[m][n]; // arreglo de dos dimensiones para guardar notas de estudiantes.
     // pedir nombres y notas
-    float notafinal[m];
-    for (int i=0;i<m;i++)
+ 
+    for (int i = 0; i < m; i++)
     {
-        cout << "Nombre del estudiante " << i+1 << ": ";
+        cout << "Nombre del estudiante " << i + 1 << ": ";
         cin >> nombres[i];
-        cout << nombres[i] <<" P1: ";
+        cout << nombres[i] << " P1: ";
         cin >> notas[i][0];
-        cout << nombres[i] <<" P2: ";
+        cout << nombres[i] << " P2: ";
         cin >> notas[i][1];
-        cout << nombres[i] <<" C1: ";
+        cout << nombres[i] << " C1: ";
         cin >> notas[i][2];
-        cout << nombres[i] <<" C2: ";
+        cout << nombres[i] << " C2: ";
         cin >> notas[i][3];
-        cout << nombres[i] <<" Lab: ";
+        cout << nombres[i] << " Lab: ";
         cin >> notas[i][4];
-        cout << nombres[i] <<" Proyecto: ";
+        cout << nombres[i] << " Proyecto: ";
         cin >> notas[i][5];
     }
-    notafinal = calculo_notafin(notas,Ponderaciones);
+    float *notafinal = calculo_notafin(notas, Ponderaciones, m);
 
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < m; i++)
     {
-        cout << nombres[i] << " su nota final es: " << notafinal[i] << "\n";
+        cout << nombres[i] << " su nota final es: " << notas[i][6] << "\n";
     }
 
-
-
-
-    instruccion();
+    
     return 0;
 }
